506e36f24014688cea3a9f2be242fd75d9cda410
Devin <120550670+Flow1t@users.noreply.github.com>
2025-03-08 10:28:02 +0700

    Add files via upload
    Added home page, a sidebar with 3 menus: Home, Mutasi Bank, Pencairan.

---
+++ Main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Metro Loan App"
+)
+
+st.write("# TXT to CSV Parser 🔥")
+
+st.sidebar.success("Select a feature above.")
+
+st.markdown(
+    """
+    ### Convert your existing txt file into csv!
+    Features:
+    - Mutasi Bank
+    - Pencairan
+    \n\n
+    Created by: Devin Augustin
+    """
+)
