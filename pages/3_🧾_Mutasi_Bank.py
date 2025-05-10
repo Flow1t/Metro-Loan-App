2a9eaf556cc00c0bc76af32b654f506874887dc7
Flow1t <flowit000@gmail.com>
2025-05-10 10:15:10 +0700

    updated features' name and added new feature

---
+++ pages/3_🧾_Mutasi_Bank.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import re
+
+st.set_page_config(page_title="Mutasi")
+
+st.sidebar.header("Mutasi Bank Organizer")
+
+def mutasi_organizer(file1):
+    df = pd.read_excel("contoh_mutasi_bank.xlsx", index_col=0)
+
+    df['Debit'] = np.where(df['Jenis Transaksi'] == 'DB', df['Mutasi'], 0)
+    df['Kredit'] = np.where(df['Jenis Transaksi'] == 'CR', df['Mutasi'], 0)
+
+    df1 = df1.drop(columns=['Mutasi', 'Jenis Transaksi', 'Cabang'])
+
+    new_columns_order = [
+        'Keterangan',
+        'Debit',
+        'Kredit',
+        'Saldo'
+    ]
+
+    mutasi_final = df1.reindex(columns=new_columns_order, fill_value=0)
+    return mutasi_final
+
+
+def main():
+    st.title("Mutasi Bank Organizer")
+    st.write("Upload your Mutasi Bank file. The app will seperate the Debit and Credit automatically")
+
+    file1 = st.file_uploader("Choose a TXT file", type=["xlsx"])
+
+    if file1 is not None:
+        mutasi = mutasi_organizer(file1)
+
+        st.success("Mutasi Bank File Generated")
+        
+        # Read the generated Excel file in binary mode
+        with open(mutasi, "rb") as f:
+            file_content = f.read()
+        
+        st.download_button("Download Mutasi Bank File", data=file_content, file_name="mutasi_bank.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
