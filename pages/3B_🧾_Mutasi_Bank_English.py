6a568bf3384750302d743b21e0b4da1917c63007
Flow1t <flowit000@gmail.com>
2025-05-23 09:47:47 +0700

    fix

--- pages/3B_🧾_Mutasi_Bank_English.py
+++ pages/3B_🧾_Mutasi_Bank_English.py
@@ -46,7 +46,7 @@ def main():
         st.download_button(
             "Download Mutasi Bank File",
             data=processed_data,
-            file_name=mutasi,
+            file_name=file_mutasi,
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
 
