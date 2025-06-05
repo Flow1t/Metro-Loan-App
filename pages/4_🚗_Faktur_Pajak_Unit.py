5ca765356d7c9f9a92864bf2ae4dc9a81ccf6ece
Flow1t <flowit000@gmail.com>
2025-06-05 14:12:36 +0700

    Update: Added feature faktur

--- pages/4_🚗_Faktur_Pajak_Unit.py
+++ pages/4_🚗_Faktur_Pajak_Unit.py
@@ -19,7 +19,7 @@ def pajak_unit(file1):
     mask_mk = df['ColumnB'].astype(str).str.startswith('MK')
     mask_non_mk = ~mask_mk
 
-    pattern_mk = r"seri faktur pajak|900|901|total ppn|dasar pengenaan pajak|2022|^(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30)$"
+    pattern_mk = r"seri faktur pajak|900|901|total ppn|dasar pengenaan pajak|2022|2021|2023|2024|^(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40)$"
     pattern_non_mk = r"seri faktur pajak|900|901|total ppn|dasar pengenaan pajak"
 
     # Keep rows where the column contains "seri faktur pajak" or any value with "901"
