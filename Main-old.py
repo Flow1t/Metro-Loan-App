73cb77f91f0a28ffeb3c245c382021b241269259
Flow1t <flowit000@gmail.com>
2026-02-25 14:33:34 +0700

    New UI/UX

---
+++ Main-old.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Metro Loan App"
+)
+
+st.write("# File Organiser 🗂️")
+
+st.sidebar.success("Select a feature above.")
+
+st.markdown(
+    """
+    ### Organise your file in an instant 📥✨
+    Features:
+    - Mutasi Bank MG
+    - Pencairan MG
+    - Mutasi Bank - Indonesia *NEW*
+    - Mutasi Bank - English *NEW*
+    - Faktur Pajak Unit
+    \n\n
+    Created by: ***Devin Augustin***
+    """
+)
